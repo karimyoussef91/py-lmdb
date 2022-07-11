64c2a15c3e59e90ffc4b9129ecee261db8d43817
Karim Yasser Mohamed Yousry Youssef <youssef2@mammoth35.llnl.gov>
2022-07-11 14:19:50 -0700

    Edit launch script

--- run_incremental_privateer.sh
+++ run_incremental_privateer.sh
@@ -11,14 +11,16 @@ first_file=$(head -n 1 ${INPUT_FILES_LIST})
 while IFS= read -r line
 do
     if [[ "$line" == "${first_file}" ]]; then
-        python examples/incremental_snapshots_privateer.py /p/lustre3/llamag/reddit/comments/${line} ${PRIVATEER_BASE_PATH} ${INITIAL_DATABASE_NAME} "db_${line}" "1"
+        python examples/incremental_snapshots_privateer.py /p/lustre3/youssef2/Reddit_comments_by_year/${line} ${PRIVATEER_BASE_PATH} ${INITIAL_DATABASE_NAME} "db_${line}" "1"
     else
-        python examples/incremental_snapshots_privateer.py /p/lustre3/llamag/reddit/comments/${line} ${PRIVATEER_BASE_PATH} ${INITIAL_DATABASE_NAME} "db_${line}" "0"
+        python examples/incremental_snapshots_privateer.py /p/lustre3/youssef2/Reddit_comments_by_year/${line} ${PRIVATEER_BASE_PATH} ${INITIAL_DATABASE_NAME} "db_${line}" "0"
     fi
+    du -hs ${PRIVATEER_BASE_PATH}
+    du -hs ${PRIVATEER_BASE_PATH}/blocks
+
+    du -hs --apparent-size ${PRIVATEER_BASE_PATH}
+    du -hs --apparent-size ${PRIVATEER_BASE_PATH}/blocks
+
+    du -hs ${PRIVATEER_BASE_PATH}/"db_${line}"
+    du -hs --apparent-size ${PRIVATEER_BASE_PATH}/"db_${line}"
 done < "${INPUT_FILES_LIST}"
-
-du -hs ${PRIVATEER_BASE_PATH}
-du -hs ${PRIVATEER_BASE_PATH}/blocks
-
-du -hs --apparent-size ${PRIVATEER_BASE_PATH}
-du -hs --apparent-size ${PRIVATEER_BASE_PATH}/blocks
